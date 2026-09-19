2e3f56351b81b3fd2a176e61c08e2daa22d2c82b
Liyang <weil77868@gmail.com>
2026-09-19 19:33:00 +0800

    Create A.cpp

---
+++ AtCoder/ABC475/A.cpp
@@ -0,0 +1,23 @@
+/*
+ * Problem: AtCoder ABC475 A - mnclr
+ * Link: https://atcoder.jp/contests/abc475/tasks/abc475_a
+ * Tag: Implementation, String
+ */
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  string ans = "";
+  int len = s.size();
+  for (int i = 0; i < len; i++) {
+    ans += s[i];
+    if (i < len - 1) {
+      ans += 'o';
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
